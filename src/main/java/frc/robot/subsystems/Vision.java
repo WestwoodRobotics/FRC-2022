a4a4ac0b639d91fd220d007e08feafae8b2f665c
Elaine <elaine.ren.yilan@gmail.com>
2021-10-30 16:26:46 -0500

    added vision stuff

---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
+public class Vision extends SubsystemBase {
+  /** Creates a new VisionProcessing. */
+  private NetworkTable table;
+  private NetworkTableEntry tx;
+  private NetworkTableEntry ty; 
+  private NetworkTableEntry ta;
+
+  public Vision() 
+  {
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    ta = table.getEntry("ta");
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
