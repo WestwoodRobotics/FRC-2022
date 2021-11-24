3a6c2f5459bcd597e471444f7f2f86df54dad1b5
Elaine <elaine.ren.yilan@gmail.com>
2021-11-24 02:27:48 -0600

    new updates on SwerveModule 11/24

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,15 +13,34 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final class SwerveDriveConstants
+    public static final class SwerveConstants
     {
-        public final int P_DRIVE_TALONSRX1 = 11,
-                         P_DRIVE_TALONSRX2 = 12,
-                         P_DRIVE_TALONSRX3 = 13,
-                         P_DRIVE_TALONSRX4 = 14,
-                         P_DRIVE_TALONSRX5 = 15,
-                         P_DRIVE_TALONSRX6 = 16,
-                         P_DRIVE_TALONSRX7 = 17,
-                         P_DRIVE_TALONSRX8 = 18;
+        //motor ports
+        public static final int P_DRIVE_TALONFX1 = 11,
+                                P_DRIVE_TALONFX2 = 12,
+                                P_DRIVE_TALONFX3 = 13,
+                                P_DRIVE_TALONFX4 = 14,
+                                P_DRIVE_TALONFX5 = 15,
+                                P_DRIVE_TALONFX6 = 16,
+                                P_DRIVE_TALONFX7 = 17,
+                                P_DRIVE_TALONFX8 = 18;
+
+        //motor constants
+        public static final double  C_kMaxMotorAngularSpeed = 0 * 2 * Math.PI, //rad/s
+                                    C_kMaxMotorAngularAcceleration = 0 * 2 * Math.PI; //rad/s^2
+
+        //PID constants
+        public static final double  C_SWERVE_kP = 0,
+                                    C_SWERVE_kI = 0,
+                                    C_SWERVE_kD = 0;
+        //Feedfoward constants
+        public static final double  C_SWERVE_kA = 0,
+                                    C_SWERVE_kS = 0,
+                                    C_SWERVE_kV =0;
+
+
+
     }
+
+    
 }
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -3,9 +3,19 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+
+import static frc.robot.Constants.SwerveConstants.*;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class SwerveModule extends SubsystemBase {
   /** Creates a new SwerveModule. */
@@ -13,22 +23,48 @@ public class SwerveModule extends SubsystemBase {
   public final TalonFX turningMotor;
   public final TalonFX driveMotor;
 
-  /*private Translation2d m_frontLeftLocation = new Translation2d(0, 0);
-  private Translation2d m_frontRightLocation = new Translation2d(0, -0);
-  private Translation2d m_backLeftLocation = new Translation2d(-0, 0);
-  private Translation2d m_backRightLocation = new Translation2d(-0, -0);
-  private SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
-                                                m_frontLeftLocation, 
-                                                m_frontRightLocation, 
-                                                m_backLeftLocation, 
-                                                m_backRightLocation);
-  */
-  public SwerveModule(int moduleNum, TalonFX turningMotor, TalonFX driveMotor) {
-    this.moduleNum = moduleNum;
-    this.turningMotor = turningMotor;
-    this.driveMotor = driveMotor;
+  private final PIDController driveMotorPID = new PIDController(C_SWERVE_kP, C_SWERVE_kI, C_SWERVE_kD);
+  private final ProfiledPIDController turnMotorPID =
+                                                  new ProfiledPIDController(C_SWERVE_kP, C_SWERVE_kI, C_SWERVE_kD, 
+                                                  new TrapezoidProfile.Constraints(C_kMaxMotorAngularSpeed, C_kMaxMotorAngularAcceleration));
 
+  private final SimpleMotorFeedforward driveFeedforward = new SimpleMotorFeedforward(C_SWERVE_kA, C_SWERVE_kS, C_SWERVE_kV);
+  private final SimpleMotorFeedforward turnFeedforward = new SimpleMotorFeedforward(C_SWERVE_kA, C_SWERVE_kS, C_SWERVE_kV);
+  
+  //constructor 
+  public SwerveModule(int moduleNum, TalonFX driveMotor, TalonFX turningMotor, boolean invertDrive, boolean invertTurn) {
+    this.moduleNum = moduleNum;
+    this.driveMotor = driveMotor;
+    this.turningMotor = turningMotor;
+    
+    //reset encoders
+    driveMotor.setSelectedSensorPosition(0);
+    turningMotor.setSelectedSensorPosition(0);
+
+    driveMotor.setInverted(invertDrive);
+    turningMotor.setInverted(invertTurn);
+
+    driveMotor.setNeutralMode(NeutralMode.Brake);
+    turningMotor.setNeutralMode(NeutralMode.Brake);
+
+    //set the PID controller input range form -pi to pi
+    turnMotorPID.enableContinuousInput(-Math.PI, Math.PI);
   }
+
+  //set encoder position of both motors to 0
+  public void resetEncoders()
+  {
+    turningMotor.setSelectedSensorPosition(0);
+    driveMotor.setSelectedSensorPosition(0);
+  }
+
+  
+
+
+  /*public Rotation2d getHeading()
+  {
+    //return new Rotaion2d ();
+  }*/
 
   @Override
   public void periodic() {
