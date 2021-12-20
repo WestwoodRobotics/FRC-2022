83df5c7748ec288c235e7d6ac6590a97097e7381
Elaine <elaine.ren.yilan@gmail.com>
2021-12-20 13:31:28 -0600

    updated swerve module 12/20

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,13 +4,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.util.Units;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
     public static final class SwerveConstants
@@ -24,6 +28,19 @@ public final class Constants {
                                 P_DRIVE_TALONFX6 = 16,
                                 P_DRIVE_TALONFX7 = 17,
                                 P_DRIVE_TALONFX8 = 18;
+
+        //module constants
+        public static final double  C_kDRIVE_MOTOR_GEAR_RATIO = 0,
+                                    C_kTURNING_MOTOR_GEAR_RATIO = 0,
+                                    C_kWHEELS_DIAMETER_METERS = Units.inchesToMeters(0);
+        
+        public static final int C_kENCODER_CPR = 0;
+
+        public static final double  C_kDRIVE_ENCODER_DISTANCE_PER_PULSE =
+                                    (C_kWHEELS_DIAMETER_METERS * Math.PI) / ((double) C_kENCODER_CPR * C_kDRIVE_MOTOR_GEAR_RATIO),
+                                    C_kTURNING_ENCODER_DISTANCE_PER_PULSE =
+                                    // Assumes the encoders are on a 1:1 reduction with the module shaft.
+                                    (2.0 * Math.PI) / (C_kENCODER_CPR* C_kTURNING_MOTOR_GEAR_RATIO);
 
         //motor constants
         public static final double  C_kMaxMotorAngularSpeed = 0 * 2 * Math.PI, //rad/s
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -4,35 +4,54 @@
 
 package frc.robot.subsystems;
 
-import static frc.robot.Constants.SwerveConstants.*;
+import static frc.robot.Constants.SwerveConstants.C_SWERVE_kA;
+import static frc.robot.Constants.SwerveConstants.C_SWERVE_kD;
+import static frc.robot.Constants.SwerveConstants.C_SWERVE_kI;
+import static frc.robot.Constants.SwerveConstants.C_SWERVE_kP;
+import static frc.robot.Constants.SwerveConstants.C_SWERVE_kS;
+import static frc.robot.Constants.SwerveConstants.C_SWERVE_kV;
+import static frc.robot.Constants.SwerveConstants.C_kDRIVE_ENCODER_DISTANCE_PER_PULSE;
+import static frc.robot.Constants.SwerveConstants.C_kMaxMotorAngularAcceleration;
+import static frc.robot.Constants.SwerveConstants.C_kMaxMotorAngularSpeed;
+import static frc.robot.Constants.SwerveConstants.C_kTURNING_MOTOR_GEAR_RATIO;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
-public class SwerveModule extends SubsystemBase {
+public class SwerveModule extends SubsystemBase 
+{
   /** Creates a new SwerveModule. */
   private int moduleNum;
   public final TalonFX turningMotor;
   public final TalonFX driveMotor;
+ 
+  private double driveMotorOutput;
+  private double turningMotorOutput;
 
   private final PIDController driveMotorPID = new PIDController(C_SWERVE_kP, C_SWERVE_kI, C_SWERVE_kD);
   private final ProfiledPIDController turnMotorPID =
                                                   new ProfiledPIDController(C_SWERVE_kP, C_SWERVE_kI, C_SWERVE_kD, 
                                                   new TrapezoidProfile.Constraints(C_kMaxMotorAngularSpeed, C_kMaxMotorAngularAcceleration));
 
-  private final SimpleMotorFeedforward driveFeedforward = new SimpleMotorFeedforward(C_SWERVE_kA, C_SWERVE_kS, C_SWERVE_kV);
-  private final SimpleMotorFeedforward turnFeedforward = new SimpleMotorFeedforward(C_SWERVE_kA, C_SWERVE_kS, C_SWERVE_kV);
+  private final SimpleMotorFeedforward m_driveFeedforward = new SimpleMotorFeedforward(C_SWERVE_kA, C_SWERVE_kS, C_SWERVE_kV);
+  private final SimpleMotorFeedforward m_turnFeedforward = new SimpleMotorFeedforward(C_SWERVE_kA, C_SWERVE_kS, C_SWERVE_kV);
   
+  Pose2d swerveModulePose = new Pose2d();
   //constructor 
-  public SwerveModule(int moduleNum, TalonFX driveMotor, TalonFX turningMotor, boolean invertDrive, boolean invertTurn) {
+  public SwerveModule(int moduleNum, TalonFX driveMotor, TalonFX turningMotor, boolean invertDrive, boolean invertTurn) 
+  {
     this.moduleNum = moduleNum;
     this.driveMotor = driveMotor;
     this.turningMotor = turningMotor;
@@ -58,16 +77,70 @@ public class SwerveModule extends SubsystemBase {
     driveMotor.setSelectedSensorPosition(0);
   }
 
-  
-
-
-  /*public Rotation2d getHeading()
+  public Rotation2d getHeading()
   {
-    //return new Rotaion2d ();
-  }*/
+    return new Rotation2d(getTurningRadians());
+  }
+
+  public double getTurningRadians() 
+  {
+      return turningMotor.getSelectedSensorPosition() * C_kTURNING_MOTOR_GEAR_RATIO;
+    
+  }
+
+  public double getTurnAngle() 
+  {
+    return Units.radiansToDegrees(getTurningRadians());
+  }
+
+  public double getVelocity()
+  {
+    return driveMotor.getSelectedSensorVelocity() * C_kDRIVE_ENCODER_DISTANCE_PER_PULSE * 10;
+  }
+
+  public SwerveModuleState getState()
+  {
+    return new SwerveModuleState(getVelocity(), new Rotation2d(getTurningRadians()));
+  }
+
+  public void setDesiredState(SwerveModuleState state)
+  {
+    SwerveModuleState outputState = SwerveModuleState.optimize(state, new Rotation2d(getTurningRadians()));
+
+    driveMotorOutput = driveMotorPID.calculate(getVelocity(), outputState.speedMetersPerSecond);
+    turningMotorOutput = turnMotorPID.calculate(getTurnAngle(), outputState.angle.getRadians());
+
+    double driveFeedforward = m_driveFeedforward.calculate(state.speedMetersPerSecond);
+    double turnFeedforward = m_turnFeedforward.calculate(turnMotorPID.getSetpoint().velocity);
+
+    driveMotor.set(ControlMode.PercentOutput, driveMotorOutput + driveFeedforward);
+    turningMotor.set(ControlMode.PercentOutput, turningMotorOutput);
+  }
+
+  public void setPercentOutput(double speed) 
+  {
+    driveMotor.set(ControlMode.PercentOutput, speed);
+  }
+
+  public void setBrakeMode(boolean mode) 
+  { // True is brake, false is coast
+    driveMotor.setNeutralMode(mode ? NeutralMode.Brake : NeutralMode.Coast);
+    turningMotor.setNeutralMode(NeutralMode.Brake);
+  }
+  
+  public Pose2d getPose() 
+  {
+    return swerveModulePose;
+  }
+
+  public void setPose(Pose2d pose) 
+  {
+    swerveModulePose = pose;
+  }
 
   @Override
-  public void periodic() {
+  public void periodic() 
+  {
     // This method will be called once per scheduler run
   }
 }
