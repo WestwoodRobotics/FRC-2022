38551b60c20e1abad08e8cfafc613ed103d5d8a4
TangentLime <tl.tangentlime@gmail.com>
2022-02-22 18:02:29 -0600

    work

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,31 +4,43 @@
 
 package frc.robot.subsystems;
 
+import java.lang.invoke.ConstantBootstraps;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.IntakeConstants.*;
+import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
+
+  private final CANSparkMax beltMotor = new CANSparkMax(P_BELT_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax flywheelMotor = new CANSparkMax(P_FLYWHEEL_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax pinionMotor = new CANSparkMax(P_PINION_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+
   /** Creates a new intake. */
   public Intake() {
-    
-    public void pushIntake(){
-      intakeSol1.set(true);
-    }
-  
-    public void pullIntake(){
-      intakeSol1.set(false);
-    }
-  
-    public void intakeStop(){
-      intakeMotor1.stopMotor();
-    }
-  
-    public void intakeIn(){
-      intakeMotor1.set(C_INTAKE_SPEED);
-    }
-  
-    public void intakeOut(){
-      intakeMotor1.set(-C_INTAKE_SPEED);
-    }
+  }
+
+  public void pushIntake() {
+    pinionMotor.set(true);
+  }
+
+  public void pullIntake() {
+    intakeSol1.set(false);
+  }
+
+  public void intakeStop() {
+    intakeMotor1.stopMotor();
+  }
+
+  public void intakeIn() {
+    intakeMotor1.set(C_INTAKE_SPEED);
+  }
+
+  public void intakeOut() {
+    intakeMotor1.set(-C_INTAKE_SPEED);
   }
 
   @Override
