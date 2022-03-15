c1aabb994c5ae8eba047101948622c537506b7f5
Aanchal <saveaanchal@gmail.com>
2022-03-15 17:17:33 -0500

    shooter functionality

---
+++ src/main/java/frc/robot/commands/shooter/ShooterToggleCommand.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.shooter;
+
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import static frc.robot.Constants.ShooterConstants.*;
+
+/** An example command that uses an example subsystem. */
+public class ShooterToggleCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Shooter m_shooter;
+  private double angle;
+  
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ShooterToggleCommand(Shooter subsystem) {
+    m_shooter = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+    if (m_shooter.getShooterVel() > 0.2)
+        m_shooter.setShooterVelocityPID(0);
+    else
+        m_shooter.setShooterVelocityPID(6000);
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+    }
+}
